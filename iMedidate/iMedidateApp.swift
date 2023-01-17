59a57af06e0f619b04badf9f9b116a256a88adb3
amirkaidarov <KaidarovAmir1441@gmail.com>
2023-01-17 18:54:05 -0500

    Initial Commit

---
+++ iMedidate/iMedidateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMedidateApp.swift
+//  iMedidate
+//
+//  Created by Амир Кайдаров on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct iMedidateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
